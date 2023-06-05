ea1b4c0f31ab2790db60db9e96a42466221b1523
Michał Osiński <nicraft@tlen.pl>
2023-06-05 20:05:50 +0200

    merge

--- src/main.d.ts
+++ src/main.d.ts
@@ -37,7 +37,7 @@ export declare class InterpreterIc10 {
     constructor(code?: string, settings?: Partial<InterpreterIc10Settings>);
     setSettings(settings?: Partial<InterpreterIc10Settings>): InterpreterIc10;
     init(text: string, device?: Device): InterpreterIc10;
-    splitString(str: string): any[];
+    splitString(str: string): string[];
     __updateDevice(): void;
     stop(): InterpreterIc10;
     run(): Promise<unknown>;
