7a56e4e832361649424b9a8c28adb579e4fdfe04
Michał Osiński <nicraft@tlen.pl>
2023-06-05 19:44:51 +0200

    separated Device from InterpreterIc10

---
+++ src/Ports.d.ts
@@ -0,0 +1,14 @@
+import { Device } from "./Device";
+export declare class Ports {
+    d0?: Device;
+    d1?: Device;
+    d2?: Device;
+    d3?: Device;
+    d4?: Device;
+    d5?: Device;
+    db: Device<"Error">;
+    constructor();
+    get(d: "db"): Device;
+    get(d: string): Device | undefined;
+    set(d: string, device: Device): Device<string>;
+}
