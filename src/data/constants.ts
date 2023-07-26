42230cb02cd3f9c71bf21f0c50817ee8d542746f
Kirill <traineratwot@yandex.ru>
2023-07-26 17:36:01 +0300

    update

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,11 +1,13 @@
+
 export default {
-    "deg2rad": "0.017453292519943295769236907684",
-    "nan": "NaN",
-    "ninf": "-Infinity",
-    "pi": "3.14159265358979",
-    "pinf": "Infinity",
-    "rad2deg": "57.2957795130823208767981",
-    "Contents": "0",
-    "Required": "1",
-    "Recipe": "2"
-} as const
+  "Contents": "",
+  "deg2rad": "0.017453292519943295769236907684",
+  "nan": "NaN",
+  "ninf": "-Infinity",
+  "pi": "3.14159265358979",
+  "pinf": "Infinity",
+  "rad2deg": "57.2957795130823208767981",
+  "Recipe": "2",
+  "Required": "1"
+} as const;
+    
